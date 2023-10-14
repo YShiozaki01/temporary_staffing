88827e6979a5bff9f0ce3d59cdf99704ad5328cd
yukio_s <ysuikoi01@gmail.com>
2023-10-14 16:01:57 +0900

    feat: 登録する人材をプルダウンメニューで選択する機能を作成

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 DATABASE = "./database/database.db"
 
+global record
+global record2
+record = {}
+record2 = {}
+
 
 def get_db_connection():
     connection = sqlite3.connect(DATABASE)
@@ -13,15 +18,23 @@ def get_db_connection():
 
 
 # リストを取得
-def get_pulldown_list(name, dept_cd, vendor_cd):
+def get_pulldown_list(name):
+    dept_cd = record["dept_cd"] if "dept_cd" in record else "%"
+    vendor_cd = record["vendor_cd"] if "vendor_cd" in record else 0
+    keyword = record["keyword"] if "keyword" in record else "%"
     sql_dept = """
         SELECT コード, 名称 FROM MDepartment;
         """
     sql_vendor = f"""
-        SELECT コード, 名称 FROM MVendor WHERE 部門 like '{dept_cd}';
+        SELECT コード, 名称 FROM MVendor WHERE 部門 = '{dept_cd}';
         """
     sql_human = f"""
-        SELECT コード, 名称 FROM MHuman_resources WHERE 業者コード like {vendor_cd};
+        SELECT コード, 名称 FROM MHuman_resources
+        WHERE 業者コード = {vendor_cd};
+        """
+    sql_human_2 = f"""
+        SELECT コード, 名称 FROM MHuman_resources
+        WHERE 業者コード = {vendor_cd} AND 名称 like '%{keyword}%';
         """
     if name == "dept":
         sql = sql_dept
@@ -29,25 +42,29 @@ def get_pulldown_list(name, dept_cd, vendor_cd):
         sql = sql_vendor
     elif name == "human":
         sql = sql_human
+    elif name == "human2":
+        sql = sql_human_2
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute(sql)
-    dept_list = cur.fetchall()
-    return dept_list
+    pulldown_list = cur.fetchall()
+    return pulldown_list
 
 
 # 表示用の名称を取得
-def get_display_name(name, code):
+def get_display_name(name):
     if name == "dept":
         table_name = "MDepartment"
+        code = record["dept_cd"]
     elif name == "vendor":
         table_name = "MVendor"
+        code = record["vendor_cd"]
     elif name == "human":
         table_name = "MHuman_resources"
+        code = record["human_cd"]
     sql = f"""
         SELECT * FROM {table_name} WHERE コード = '{code}';
         """
-    print(sql)
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute(sql)
@@ -56,26 +73,75 @@ def get_display_name(name, code):
     return display_name
 
 
+# 人材の情報を取得
+def get_human_info(code):
+    sql = f"""
+        SELECT * FROM MHuman_resources WHERE コード = '{code}';
+        """
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(sql)
+    result = cur.fetchone()
+    return result
+
+
 @app.route("/", methods=["get", "post"])
 def index():
-    record = {}
-    record["dept_cd"] = "%"
-    record["vendor_cd"] = 0
-    record["human_cd"] = 0
-    record["dept_list"] = get_pulldown_list("dept", record["dept_cd"], record["vendor_cd"])
-    record["dept_name"] = ""
-    record["vendor_list"] = get_pulldown_list("vendor", record["dept_cd"], record["vendor_cd"])
-    record["vendor_name"] = ""
-    record["human_list"] = get_pulldown_list("human", record["dept_cd"], record["vendor_cd"])
-    record["human_name"] = ""
-    if request.form.get("dpt1") == "submit1":
-        record["dept_cd"] = request.form.get("code_dept")
-        print(record["dept_cd"])
-    if request.form.get("dpt2") == "submit2":
-        record["dept_name"] = get_display_name("dept", record["dept_cd"])
-        record["vendor_list"] = get_pulldown_list("vendor", record["dept_cd"], record["vendor_cd"])
-        record["vendor_cd"] = request.form.get("code_vendor") if request.form.get("code_vendor") else ""
-        # record["vendor_name"] = get_display_name("vendor", record["vendor_cd"])
-        # record["human_list"] = get_pulldown_list("human", record["dept_cd"], record["vendor_cd"])
-    print(f"部門: {record['dept_name']}, 業者: {record['vendor_name']}")
+    record["dept_list"] = get_pulldown_list("dept")
     return render_template("index.html", record=record)
+
+
+@app.route("/select_1", methods=["get", "post"])
+def select_1():
+    record["dept_cd"] = request.form.get("code_dept")
+    if "dept_cd" in record:
+        record["dept_name"] = get_display_name("dept")
+        record["vendor_list"] = get_pulldown_list("vendor")
+    return render_template("index.html", record=record)
+
+
+@app.route("/select_2", methods=["get", "post"])
+def select_2():
+    record["vendor_cd"] = request.form.get("code_vendor")
+    if "vendor_cd" in record:
+        record["vendor_name"] = get_display_name("vendor")
+        record["human_list"] = get_pulldown_list("human")
+    return render_template("index.html", record=record)
+
+
+@app.route("/keyword", methods=["post"])
+def keyword():
+    record["keyword"] = request.form.get("keywd")
+    record["human_list"] = get_pulldown_list("human2")
+    return render_template("index.html", record=record)
+
+
+@app.route("/select_3", methods=["get", "post"])
+def select_3():
+    record["human_cd"] = request.form.get("code_human")
+    if "human_cd" in record:
+        record["human_name"] = get_display_name("human")
+        human_info = get_human_info(record["human_cd"])
+        record2["info_1"] = human_info["勤務帯"]
+        record2["info_2"] = human_info["業務内容"]
+        record2["info_3"] = human_info["基本単価"]
+    start_flg = True
+    return render_template("index.html", record=record,
+                           record2=record2, start_flg=start_flg)
+
+
+@app.route("/cancel_human", methods=["post"])
+def cancel_human():
+    print("human_cd" in record)
+    if request.form.get("cancel") == "c":
+        start_flg = False
+        del record["human_cd"]
+        print("human_cd" in record)
+        record["human_list"] = get_pulldown_list("human")
+        return render_template("index.html", record=record,
+                               start_flg=start_flg)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port="5000")
